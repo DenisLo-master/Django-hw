ab489c39bfcfa20ba4886fb75c048581d7aeb5f2
DenisLo-master <denis.lkg@gmail.com>
2023-01-20 19:55:33 +0000

    lesson_3 New context processor

--- config/settings.py
+++ config/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "mainapp.context_processor.example.simple.context_processor",
+                "mainapp.context_processor.example.simple_context_processor",
             ],
         },
     },
